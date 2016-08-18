b13c177090c2f14f67e43994db4e898023fa782c
Frank Chen H <frank.h.chen@ericsson.com>
2016-08-18 15:17:30 +0800

    add lombok

--- src/main/java/com/chenhm/springdemo/todos/Todo.java
+++ src/main/java/com/chenhm/springdemo/todos/Todo.java
@@ -1,8 +1,6 @@
 package com.chenhm.springdemo.todos;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
+import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -10,6 +8,7 @@ import java.io.Serializable;
  * Created by Frank on 8/13/2016.
  */
 @Entity
+@Data
 public class Todo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,51 +21,4 @@ public class Todo implements Serializable {
 
 	private boolean complete;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public boolean isComplete() {
-		return complete;
-	}
-	
-	public void setComplete(boolean complete) {
-		this.complete = complete;
-	}
-
-	public Todo() {
-	}
-
-	public Todo(Long id, String description, boolean complete) {
-		this.id = id;
-		this.description = description;
-		this.complete = complete;
-	}
-	
-	@Override
-	public String toString() {
-		return "[ id=" + this.id + ", description=" + this.description + ", complete=" + this.complete + " ]";
-	}
-
-	@Override
-	public boolean equals(Object other) {
-		return EqualsBuilder.reflectionEquals(this, other);
-	}
-	
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
 }
